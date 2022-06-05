52ef599c7dfb4cd3fdc790566bb57ea45884fb47
AS0530 <103614159+AS0530@users.noreply.github.com>
2022-06-05 12:10:07 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+    let img;
+
+    function img1(){
+    img = document.getElementById("image_file");
+    img.src = "image/1.jpg";
+    }
+    
+
+    function img2(){
+    img = document.getElementById("image_file");
+    img.src = "image/2.jpg";
+    }
+
+    function img3(){
+    img = document.getElementById("image_file");
+    img.src = "image/3.jpg" ;
+    }
+
+    function img4(){
+        img = document.getElementById("image_file");
+        img.src = "image/まとめ.jpg";
+        }
